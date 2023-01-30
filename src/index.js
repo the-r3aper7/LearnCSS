3320b891cbedabec4f82194d21824cc9ed93471b
the-r3aper7 <saisrikardumpeti@gmail.com>
2023-01-30 22:19:13 +0530

    small changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-const options = {
-    root: null,
-    rootMargin: '0px 0px -100px 0px',
-    threshold: [0, 0.5, 1]
-}
-
 // Create the intersection observer
 const observer = new IntersectionObserver(async (entries, ) => {
     // Check if the element is intersecting (visible)
@@ -11,15 +5,11 @@ const observer = new IntersectionObserver(async (entries, ) => {
         if (element.isIntersecting) {
             // await new Promise(r => setTimeout(r, 2000));
             element.target.classList.add("show")
-            console.log(element);
-            console.log("Element is now visible!");
         } else {
-            console.log(element);
             element.target.classList.remove("show")
-            console.log("Element is no longer visible!");
         }
     })
-}, options);
+});
 
 // Select the element to observe
 const hiddenElements = document.querySelectorAll(".hidden");
